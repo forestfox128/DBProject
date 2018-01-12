6159f4b69c68bcfc8805769bd7aff4e22fa75257
Maciej Hajduk <ebrithil97@gmail.com>
2018-01-12 15:24:18 +0100

    klients crowler

--- src/routes/klienci.js
+++ src/routes/klienci.js
@@ -1,19 +1,19 @@
 exports.list = function (req, res) {
 
     req.getConnection(function (err, connection) {
-        var nazwa = req.query.user;
+        var surname = req.query.user;
 
         var filter = "";
-        if (nazwa) {
+        if (surname) {
             filter = 'WHERE nazwisko = ?';
         }
 
-        var query = connection.query('SELECT * FROM Klient ' + filter, nazwa, function (err, rows) {
+        var query = connection.query('SELECT * FROM Klient ' + filter, surname, function (err, rows) {
             if (err) {
                 console.log("Error Selecting : %s ", err);
             }
 
-            res.render('klienci', {page_title: 'AlkoSZOP - Klienci', data: rows, user: nazwa});
+            res.render('klienci', {page_title: 'AlkoSZOP - Klienci', data: rows, user: surname});
         });
     });
 };
